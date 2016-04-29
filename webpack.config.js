1b4a25fdb3dcfeb0147f9981e16db701b3d10a6c
Gloria Ma <gloria.ma@aya.yale.edu>
2016-04-28 22:09:16 -0700

    Add comments to webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,20 @@
 module.exports = {
-  entry: [
-    './client/index.js'
-  ],
+  entry: './client/index.js', // update this depending on file structure
   output: {
-    path: __dirname,
-    publicPath: '/',
+    path: __dirname, // path to place output
+    publicPath: '/', // path for <script> tag to include output
     filename: 'bundle.js'
   },
   module: {
-    loaders: [{
+    loaders: [{ // use Babel to transpile all files except node_modules
       exclude: /node_modules/,
       loader: 'babel'
     }]
   },
   resolve: {
-    extensions: ['', '.js', '.jsx']
+    extensions: ['', '.js', '.jsx'] // extensions used to resolve modules
   },
-  devServer: {
+  devServer: { // settings for webpack-dev-server
     historyApiFallback: true,
     contentBase: './'
   }
